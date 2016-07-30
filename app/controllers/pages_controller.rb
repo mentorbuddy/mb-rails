67f8055e727d729c5de1236083dbab28b5064d1f
Viresh Dhawan <viresh.dhawan@gmail.com>
2016-07-30 14:21:39 +0530

    Added post form to components subdirectory.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,7 @@ class PagesController < ApplicationController
 
   # back-end code for pages/home
   def home
+    @Posts = Post.all
   end
 
   # back-end code for pages/profile
@@ -18,10 +19,14 @@ class PagesController < ApplicationController
       # Redirect to a 404 {root for now}
       redirect_to root_path, :notice => "User not found!"
     end
+
+    @Posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+    @newPost = Post.new
   end
 
   # back-end code for pages/explore
   def explore
+    @Posts = Post.all
   end
 
 end
